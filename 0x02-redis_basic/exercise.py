6002f5cc77135c3008b4c313f83be6d922cf6366
mburuxx <njihiamburu.3@gmail.com>
2024-07-20 11:26:53 +0300

    Fix changes

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Create a Cache class. In the __init__ method
+Module to create a Cache class and implement replay functionality.
 """
 import redis
 from uuid import uuid4
@@ -9,13 +9,16 @@ from functools import wraps
 
 
 def call_history(method: Callable) -> Callable:
+    """
+    Decorator to store the history of inputs and outputs for a particular method.
+    """
     method_key = method.__qualname__
     inputs = method_key + ':inputs'
     outputs = method_key + ':outputs'
 
     @wraps(method)
     def wrapper(self, *args, **kwds):
-        """ function of the wrapper """
+        """ Wrapper function to log inputs and outputs. """
         self._redis.rpush(inputs, str(args))
         data = method(self, *args, **kwds)
         self._redis.rpush(outputs, str(data))
@@ -24,25 +27,29 @@ def call_history(method: Callable) -> Callable:
 
 
 def count_calls(method: Callable) -> Callable:
-    
+    """
+    Decorator to count the number of times a method is called.
+    """
     method_key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwds):
-        """function of the wrapper """
+        """ Wrapper function to increment the call count. """
         self._redis.incr(method_key)
         return method(self, *args, **kwds)
     return wrapper
 
 
 def replay(method: Callable):
-    """ function that display the call history """
+    """
+    Function to display the history of calls to a particular method.
+    """
     method_key = method.__qualname__
     inputs = method_key + ":inputs"
     outputs = method_key + ":outputs"
     redis = method.__self__._redis
     count = redis.get(method_key).decode("utf-8")
-    print("{} was call {} times:".format(method_key, count))
+    print("{} was called {} times:".format(method_key, count))
     ListInput = redis.lrange(inputs, 0, -1)
     ListOutput = redis.lrange(outputs, 0, -1)
     allData = list(zip(ListInput, ListOutput))
@@ -53,18 +60,20 @@ def replay(method: Callable):
 
 class Cache:
     """
-    store information on redis
+    Cache class to store information in Redis.
     """
 
     def __init__(self):
-        """reate an instance"""
+        """ Create an instance of Cache. """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ store the data and return a new uuid"""
+        """
+        Store data in Redis and return a new UUID.
+        """
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
@@ -72,16 +81,18 @@ class Cache:
     def get(self,
             key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        """ get the element"""
+        """
+        Retrieve data from Redis.
+        """
         data = self._redis.get(key)
-        if (fn is not None):
+        if fn is not None:
             return fn(data)
         return data
 
     def get_str(self, data: str) -> str:
-        """ return byte in string """
+        """ Convert bytes to string. """
         return data.decode('utf-8')
 
     def get_int(self, data: str) -> int:
-        """ return byte in integer """
+        """ Convert bytes to integer. """
         return int(data)
